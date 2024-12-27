97c0e62018182b1befe8e5aaf258bc552bfe3011
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-12-27 13:35:45 +0000

    ✨ Added core plugins

---
+++ src/plugins/signals/index.ts
@@ -0,0 +1,32 @@
+import { Plugin } from '../../types.js';
+
+export type SignalDefinition = {
+    signal: string;
+    handler: string;
+    plugin: string;
+};
+
+/**
+ * Signals extension point.
+ */
+export interface SignalsExtensionPoint {}
+
+export class SignalsPlugin extends Plugin {
+    signals: { [key: string]: SignalDefinition } = {};
+    async start() {
+        super.start();
+        const connectedExtensions = this.getConnectedExtensions('signals');
+        connectedExtensions &&
+            connectedExtensions.forEach((extension) => {
+                const signalDefinition = extension.meta! as SignalDefinition[];
+                for (const item of signalDefinition) {
+                    item.plugin = extension.plugin;
+                    this.signals[item.signal] = item;
+                }
+            });
+    }
+    async stop() {}
+    getSignalDefinition(signalType: string): SignalDefinition | undefined {
+        return this.signals[signalType];
+    }
+}
